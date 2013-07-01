08d828e6ea8d5d5b2eb7c8d40fb0cb5cd14383f8
Michail Beliy <admin@sys-admin.com.ua>
2013-07-01 17:42:03 +0300

    asterisk query bugfix, cache id fix

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 include "conf/config.php";
 require_once 'Cache/Lite.php';
-$id=mktime(date("H"), floor(date("i")/10), 0, date("n"), date("j"), date("Y"));
+$id=mktime(date("H"), (floor(date("i")/10))*10, 0, date("n"), date("j"), date("Y"));
 if (isset($_GET['id'])){ $id=time();}
 
 $opts = array(
-    'cacheDir' => '/tmp/cache/',
+    'cacheDir' => './cache/',
     'automaticSerialization' => true,
     'lifeTime' => 600 );
 
@@ -29,71 +29,45 @@ function get_user_info($userid) {
     $query="SELECT `firstname`,`lastname`,`username` FROM `swstaff` where `staffid` = '".$userid."';";
     dbconnect();
     global $link;
-    $starttime = microtime(true);
+//    $starttime = microtime(true);
     $result = mysqli_query($link, $query, MYSQLI_USE_RESULT) or die("Query failed (info)[$link] : " . mysqli_error($link));
-    $endtime = microtime(true); $duration = $endtime - $starttime;
+//    $endtime = microtime(true); $duration = $endtime - $starttime;
     $row=mysqli_fetch_row($result);
-    return "".$row['0']." ".$row['1']." (".$row['2'].")<!-- ".$userid." ($duration ms) -->";
-}
-
-function get_user_id_with_max_chatobj($enabledID) {
-    $query='SELECT
-    	`staffid`
-	FROM (
-		SELECT
-			COUNT(`chatobjectid`) ccobj,
-			staffid
-		FROM
-			`swchatobjects`
-		WHERE
-			`chatstatus` = 3
-			AND
-			`staffpostactivity` > UNIX_TIMESTAMP(CURDATE())
-			AND
-			`staffid` IN (' . $enabledID . ')
-		GROUP BY
-			staffid
-	) t
-	HAVING MAX(t.ccobj)';
-    dbconnect();
-    global $link;
-    $result = mysqli_query($link, $query, MYSQLI_USE_RESULT) or die("Query failed (chat) : " . mysqli_error($link));
-    $row=mysqli_fetch_row($result);
-    return $row['0'];
+    return "".$row['0']." ".$row['1']." (".$row['2'].")<!-- ".$userid." -->";
 }
 
 function get_user_chat($userid) {
     $query="select count(`chatobjectid`) from `swchatobjects` where `chatstatus` = '3' and `staffpostactivity` > UNIX_TIMESTAMP(CURDATE()) and `staffid` = '".$userid."';";
     dbconnect();
     global $link;
-    $starttime = microtime(true);
+//    $starttime = microtime(true);
     $result = mysqli_query($link, $query, MYSQLI_USE_RESULT) or die("Query failed (chat) : " . mysqli_error($link));
-    $endtime = microtime(true); $duration = $endtime - $starttime;
+//    $endtime = microtime(true); $duration = $endtime - $starttime;
     $row=mysqli_fetch_row($result);
-    return $row['0']."<!-- $duration -->";
+    return $row['0'];
 }
 
 function get_user_ticket($userid) {
     $query="select count(*) from `swticketauditlogs` where `dateline` > unix_timestamp(curdate()) and `creatorid` = '".$userid."' and `actionmsg` = \"Ticket status changed from: Open to: In Progress\";";
     dbconnect();
     global $link;
-    $starttime = microtime(true);
+//    $starttime = microtime(true);
     $result = mysqli_query($link,$query, MYSQLI_USE_RESULT) or die("Query failed (ticket): " . mysqli_error($link));
-    $endtime = microtime(true); $duration = $endtime - $starttime;
+//    $endtime = microtime(true); $duration = $endtime - $starttime;
     $row=mysqli_fetch_row($result);
-    return $row['0']."<!-- $duration -->";
+    return $row['0'];
 }
 
 function get_user_phone($userid) {
     include "conf/config.php";
-    $query="SELECT count(*)  FROM `cdr` WHERE `calldate` > curdate() and `disposition` = 'ANSWERED' and `dstchannel` like 'SIP/".$sip[$userid]."%'";
+    $query="SELECT count(*)  FROM `cdr` WHERE `calldate` > curdate() and `disposition` = 'ANSWERED' and `dstchannel` like 'SIP/".$sip[$userid]."%' AND  `lastapp` !=  'Hangup'";
     dbas();
     global $linkas;
-    $starttime = microtime(true);
+//    $starttime = microtime(true);
     $result = mysqli_query($linkas, $query, MYSQLI_USE_RESULT) or die("Query failed (chat) : " . mysqli_error($linkas)."|||".mysqli_connect_error($linkas));
-    $endtime = microtime(true); $duration = $endtime - $starttime;
+//    $endtime = microtime(true); $duration = $endtime - $starttime;
     $row = mysqli_fetch_row($result);
-    return $row[0]."<!-- $duration -->";
+    return $row[0];
 }
 
 $Cache_Lite = new Cache_Lite($opts);
@@ -125,23 +99,15 @@ $("td[value^='" + $("td[name=phone]").max() + "']").css("background-color","#C2F
 EOH;
 # <?php
 
-$max_chatobj_user_id = get_user_id_with_max_chatobj($config['enabledID']);
-
 foreach (explode(",", $config['enabledID']) as $user) {
 $chat=get_user_chat($user);
 $ticket=get_user_ticket($user);
 $phone=get_user_phone($user);
 
 $design.="<tr><td>".get_user_info($user)."</td>";
-<<<<<<< HEAD
-if ($chat==0)	{$design.="<td bgcolor=\"#FFD9D9\">".$chat."</td>";}else { $design.="<td name=\"chat\" value=\"".$chat."\">".$chat."</td>";} 
-if ($ticket==0)	{$design.="<td bgcolor=\"#FFD9D9\">".$ticket."</td>";}else {$design.="<td name=\"ticket\" value=\"".$ticket."\">".$ticket."</td>";} 
-if ($phone==0)	{$design.="<td bgcolor=\"#FFD9D9\">".$phone."</td>";}else {$design.="<td name=\"phone\" value=\"".$phone."\">".$phone."</td>";}
-=======
-if ($chat==0)	{$design.="<td bgcolor=\"#FFD9D9\">".$chat."</td>";} elseif ((int)$user == (int)$max_chatobj_user_id) { $design.="<td bgcolor=\"#C2FFCE\">".$chat."</td>"; } else { $design.="<td>".$chat."</td>";}
-if ($ticket==0)	{$design.="<td bgcolor=\"#FFD9D9\">".$ticket."</td>";}else {$design.="<td>".$ticket."</td>";}
-if ($phone==0)	{$design.="<td bgcolor=\"#FFD9D9\">".$phone."</td>";}else {$design.="<td>".$phone."</td>";}
->>>>>>> 6a86e463b599e83bc49c7a3d8c4cdb01ce748d5b
+if ($chat==0)   {$design.="<td bgcolor=\"#FFD9D9\">".$chat."</td>";}else { $design.="<td name=\"chat\" value=\"".$chat."\">".$chat."</td>";}
+if ($ticket==0) {$design.="<td bgcolor=\"#FFD9D9\">".$ticket."</td>";}else {$design.="<td name=\"ticket\" value=\"".$ticket."\">".$ticket."</td>";}
+if ($phone==0)  {$design.="<td bgcolor=\"#FFD9D9\">".$phone."</td>";}else {$design.="<td name=\"phone\" value=\"".$phone."\">".$phone."</td>";}
 $design.=" </tr>\r\n";
 }
 
